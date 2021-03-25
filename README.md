a20a1141925e98ee5402f50c72d3e9225ef2ddf0
Adir2020 <67104058+Adir2020@users.noreply.github.com>
2021-03-25 08:59:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Web scrapping
+
+garimpar dados de outros web sites
+
+## Casos de uso
+
+-voçe precisa pegar dados de livros/roupas/imagens/materiais para montar uma loja virtual
+-saber quais os ultimos anuncios de um determinado produto numa loja virtual.
+-voçe precisa pegar dados de uma plataforma de videos (imdb)
+
+## O que vamos fazer?
+
+puxar ultimas imagens do instagram, para adicionar na nossa pagina web.
+
+## Desafios
+
+-[x] criar o backend que ira se conectar com o instagram
+-[x] garimpar os dados
+-[x] preparar os dados
+-[x] salvar os dados em um arquivo local
+-[x] consumir os dados do nosso arquivo local
+
+## Next level
+
+-[] fazer uns dos casos de uso acima
+-[] fazer o dowload de imagens e deixar em uma pasta temporaria.
---
+++ index.js
@@ -0,0 +1,37 @@
+const puppeteer = require('puppeteer');
+const fs = require("fs");
+
+(async () => {                           //nao esconder o navegador  
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://instagram.com/rocketseat_oficial');
+
+  const imgList = await page.evaluate(() => {
+      // toda essa função será executada no browser
+      
+      //vamos pegar todas as imagens que estão na parte de posts
+       const nodeList = document.querySelectorAll("article img ")
+      
+       //transformar o nodelist em array
+      const imgArray = [...nodeList]
+
+      // transformar os nodes (elementos html) em objetos js
+       const imgList = imgArray.map( ({src}) => ({
+          src
+       }))
+       
+      // colocar para fora da função
+      return imgList
+  });
+
+     // escrever os dados em um arquivo local(json)
+     fs.writeFile("instagram.json", JSON.stringify(imgList, null, 2), err => {
+         if(err) throw new Error("something went wrong")
+
+
+         console.log("well done!")
+     })
+
+
+  await browser.close();
+})();
